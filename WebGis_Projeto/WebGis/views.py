eded85cd546c296626850ad5bca5663c2fa60218
edsonflavio <edson.flavio@gmail.com>
2023-03-27 13:14:26 -0300

    Ajustes nos arquivos de views e docker-compose

--- WebGis_Projeto/WebGis/views.py
+++ WebGis_Projeto/WebGis/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-
 def index(request):
-    return HttpResponse("Hello, world. Aqui começa seu WebGIS.")
+    # Prints client IP address: "12.34.56.78"
+    IP_Cliente = request.META["HTTP_X_FORWARDED_FOR"]
+    # Prints NGINX IP address: "127.0.0.1", ie. localhost
+    IP_Nginx = request.META["REMOTE_ADDR"]
+    return HttpResponse(f'Seja bem vindo ' + IP_Cliente + ' Você está acessando via ' + IP_Nginx +'. Aqui começa seu WebGIS.')
