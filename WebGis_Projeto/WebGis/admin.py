68dd329da17072fa703290846f5f258003a01aa9
Edson Flavio de Souza <edson.flavio@ufpr.br>
2023-06-26 11:53:38 -0300

    Reconstruído as imagens Base

--- WebGis_Projeto/WebGis/admin.py
+++ WebGis_Projeto/WebGis/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.db import models
+from . import models
 
 # Register your models here.
+
+admin.site.register(models.Ocorrencia)
+admin.site.register(models.TipoOcorrencias)
--- WebGis_Projeto/WebGis/models.py
+++ WebGis_Projeto/WebGis/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class TipoOcorrencias(models.Model):
+    '''
+        Tabela de Tipo de Ocorrências
+    '''
+    toc_tipoOcorrencia = models.CharField(max_length=50)
+    toc_dataCadastro = models.DateTimeField(auto_now_add=True)
+    toc_dataAlteracao = models.DateTimeField(auto_now=True)
+    toc_ipAddress = models.GenericIPAddressField()
+    toc_user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+
+class Ocorrencia(models.Model):
+    '''
+        Tabela de Ocorrências
+    '''
+    oco_nomeOcorrencia = models.CharField(max_length=120)
+    oco_dataCadastro = models.DateTimeField(auto_now_add=True)
+    oco_dataAlteracao = models.DateTimeField(auto_now=True)
+    oco_descricaoOcorrencia = models.CharField(max_length=120)
+    oco_ipAddress = models.GenericIPAddressField()
